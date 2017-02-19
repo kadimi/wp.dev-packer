bba49af7b72a107a0d95a6fb1af0cb7055c8271f
Nabil Kadimi <nabil@kadimi.com>
2017-02-19 23:08:40 +0000

    Use Packer `-force` flag instead of gulp task `clean`

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,3 @@
-const del        = require('del');
 const download   = require('gulp-download');
 const fileExists = require('file-exists');
 const gulp       = require('gulp');
@@ -7,36 +6,30 @@ const spawn      = require('child_process').spawn;
 /**
  * Default task.
  *
- * - Delete existing artifacts
  * - Maybe download vagrant insecure key pair
  * - Build machine
  */
 gulp.task('default', () => {
-    gulp.start( 'build', ['clean', 'keys']);
+    gulp.start( 'build', ['keys']);
 });
 
 /**
  * Builds machine
+ *
+ * Uses the command `packer build -force template.json`
  */
-gulp.task('build', function () {
-  spawn('packer', ['build', 'template.json'], {stdio: 'inherit'}).on('close', function (code) {
+gulp.task('build', () => {
+  spawn('packer', ['build', '-force', 'template.json'], {stdio: 'inherit'}).on('close', function (code) {
     console.log('Build process exited with code ' + code);
   });
 });
 
 /**
- * Redowloads vagrant key pair if at least a key is missing
+ * Redownloads vagrant key pair if at least a key is missing
  */
 gulp.task('keys', () => {
     if (!fileExists.sync('keys/vagrant') || !fileExists.sync('keys/vagrant.pub')) {
         download('https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant').pipe(gulp.dest("keys/"));
         download('https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub').pipe(gulp.dest("keys/"));
     }
-});
-
-/**
- * Deletes existing artifacts
- */
-gulp.task('clean', () => {
-    del(['output-', 'packer_cache']);
 });
