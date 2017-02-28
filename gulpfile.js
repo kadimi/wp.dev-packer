f90416772bafa3bd7d1bd0ed388231ef44757b43
Nabil Kadimi <nabil@kadimi.com>
2017-02-28 07:33:54 +0000

    Add more gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const del         = require('del');
 const fileExists  = require('file-exists');
 const gulp        = require('gulp');
+const mkdirp      = require('mkdirp');
 const rename      = require('gulp-rename');
 const remoteSrc   = require('gulp-remote-src');
 const runSequence = require('run-sequence');
@@ -13,7 +14,8 @@ gulp.task('default', ()=> {
   spawn('gulp', ['clean'], {stdio: 'inherit'}).on('close', () => {
     spawn('gulp', ['dl'], {stdio: 'inherit'}).on('close', () => {
       spawn('gulp', ['berks'], {stdio: 'inherit'}).on('close', () => {
-        spawn('gulp', ['build'], {stdio: 'inherit'}).on('close', () => {
+        spawn('gulp', ['build'], {stdio: 'inherit'}).on('close', function (code) {
+          console.log('`gulp build` process exited with code ' + code);
         });
       });
     });
@@ -32,6 +34,33 @@ gulp.task('build', () => {
 });
 
 /**
+ * Add box if it exists
+ *
+ * @todo "...if exists"
+ */
+gulp.task('box-add', () => {
+  spawn('vagrant', ['box', 'add', 'packer_vagrant_virtualbox.box', '--name', 'wp.dev', '--force'], {stdio: 'inherit'}).on('close', function (code) {
+    console.log('`vagrant box add` process exited with code ' + code);
+  });
+});
+
+/**
+ * Launch a new Vagrant box
+ */
+gulp.task('box-launch', () => {
+  mkdirp('vm');
+  process.chdir('vm');
+  spawn('vagrant', ['init', 'wp.dev', '-f'], {stdio: 'inherit'}).on('close', function (code) {
+    process.chdir('vm');
+    spawn('vagrant', ['up'], {stdio: 'inherit'}).on('close', function (code) {
+      console.log('`vagrant up` process exited with code ' + code);
+    });
+    process.chdir('..');
+  });
+  process.chdir('..');
+});
+
+/**
  * Installs cookbooks
  *
  * Runs `berks vendor vendor/cookbooks` from `cookbooks/wp.dev`
@@ -45,7 +74,7 @@ gulp.task('berks', () => {
 });
 
 /**
- * Downloads vagrant key pair if at least one key is missing
+ * Downloads Vagrant key pair if at least one key is missing
  */
 gulp.task('dl-keys', () => {
   if (!fileExists.sync('vendor/vagrant/keys/vagrant') || !fileExists.sync('vendor/vagrant/keys/vagrant.pub')) {
