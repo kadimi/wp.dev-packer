4366c289e3e30bbbd4c5d852c7225770bb7746ea
Nabil Kadimi <nabil@kadimi.com>
2017-07-29 21:53:17 +0100

    Improve download tasks

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@
 const box = 'bento/ubuntu-16.04';
 
 const del         = require('del');
+const execSync    = require('child_process').execSync;
 const fileExists  = require('file-exists');
 const gulp        = require('gulp');
 const jscs        = require('gulp-jscs');
@@ -11,7 +12,21 @@ const mkdirp      = require('mkdirp');
 const rename      = require('gulp-rename');
 const remoteSrc   = require('gulp-remote-src');
 const spawn       = require('child_process').spawn;
-const execSync    = require('child_process').execSync;
+const util        = require('gulp-util');
+
+/**
+ * Simple downloader.
+ */
+const dl = function(url, name, dir='.', force) {
+  var path=`${dir}/${name}`;
+  if ( fileExists.sync(`${dir}/${name}`) && !force ) {
+    util.log(`File util.colors.cyan(path) exists, skipping...` );
+  } else {
+    util.log(`Downloading ${util.colors.cyan(url)} as ${util.colors.cyan(path)}...` );
+    execSync(`mkdir -p ${dir}`, { stdio: 'inherit' });
+    execSync(`wget -O ${dir}/${name} "${url}"`, { stdio: 'inherit' });
+  }
+};
 
 /**
  * Builds machine.
@@ -46,10 +61,7 @@ gulp.task('variables', () => {
   execSync(`echo "{\\\"box\\\":\\\"${boxPath}\\\"}" > variables.json`);
 });
 
-/**
- * Add box if it exists
- *
- * @todo "...if exists"
+/**...if exists"
  */
 gulp.task('box-add', () => {
   newBox = 'packer_virtualbox-ovf_virtualbox.box';
@@ -98,39 +110,34 @@ gulp.task('berks', () => {
 });
 
 /**
- * Downloads Vagrant key pair if at least one key is missing
+ * Downloads Vagrant key pair if at least one key is missing.
  */
 gulp.task('dl-keys', () => {
-  var vagrantKeysBase = 'https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/';
-  var key = 'vendor/vagrant/keys/vagrant';
-  var keyPub = `${key.pub}`;
-  if (!fileExists.sync(key) || !fileExists.sync(keyPub)) {
-    return remoteSrc(['vagrant', 'vagrant.pub'], { base: vagrantKeysBase })
-      .pipe(gulp.dest('vendor/vagrant/keys/'));
-  }
-
-  return true;
+  var vagrantKeysBase = 'https://github.com/mitchellh/vagrant/raw/master/keys/';
+  dl(`${vagrantKeysBase}vagrant`, 'vagrant', 'vendor/vagrant/keys');
+  dl(`${vagrantKeysBase}vagrant.pub`, 'vagrant.pub', 'vendor/vagrant/keys');
 });
 
 /**
  * Downloads chef insaller.sh
  */
 gulp.task('dl-chef', () => {
-  if (!fileExists.sync('vendor/chef/chef.deb')) {
-    execSync('mkdir -p vendor/chef');
-    var chefLink = execSync('sh scripts/get-chef-download-link.sh').toString().trim();
-    execSync(`wget -O vendor/chef/chef.deb "${chefLink}"`);
-  }
+  var chefLink = execSync('sh scripts/get-chef-download-link.sh').toString().trim();
+  dl(chefLink, 'chef.deb', 'vendor/chef');
 });
 
 /**
- * Downloads vagrant box.
+ * Downloads or updates vagrant box.
  */
 gulp.task('dl-box', () => {
-  args = `box add --provider virtualbox ${box}`.split(' ');
-  spawn('vagrant', args, { stdio: 'inherit' }).on('close', function (code) {
-    console.log('`vagrant up` process exited with code ' + code);
-  });
+  var boxExists = execSync(`vagrant box list | grep ${box} | wc -l`).toString().trim() == 1;
+  if(boxExists){
+    util.log(`Updating box ${util.colors.cyan(box)}...` );
+    execSync(`vagrant box update --provider virtualbox --box ${box}`, { stdio: 'inherit' });
+  } else {
+    util.log(`Downloading box ${util.colors.cyan(box)}...` );
+    execSync(`vagrant box download --provider virtualbox ${box}`, { stdio: 'inherit' });
+  }
 });
 
 /**
